ece130b42d9c706d2f4835bc04ae9f639ccad6af
Long Hoang Bao <longhoang0811@gmail.com>
2019-11-07 01:17:08 +0700

    add Mainboard team notebook

---
+++ Code/Trick/trick.cpp
@@ -0,0 +1,6 @@
+int __builtin_clz(int x);//number of leading zero
+int __builtin_ctz(int x);//number of trailing zero
+int __builtin_clzll(long long x);//number of leading zero
+int __builtin_ctzll(long long x);//number of trailing zero
+int __builtin_popcount(int x);// number of 1-bits in x
+int __builtin_popcountll(long long x);//number of 1-bits i
