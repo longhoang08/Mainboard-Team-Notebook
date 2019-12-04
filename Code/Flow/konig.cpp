ddc2dc7840d27fea3e50c54d4151612e20bf2bfd
thang.nguyen5 <thang.nguyen5@tiki.vn>
2019-12-05 00:32:50 +0700

    update something

--- Code/Flow/konig.cpp
+++ Code/Flow/konig.cpp
@@ -1,15 +1,12 @@
 void konig(){
    queue<int> qu;
-
    f1(i,m) if (!Assigned[i]) qu.push(i);
    f1(i,n) if (!Assigned[N-i]) qu.push(N-i);
-
    while (qu.size()){
       int u=qu.front(); qu.pop();
       for (int i=0; int v=a[u][i]; i++)
       if (!(Choosed[v]++)) qu.push(Assigned[v]);
    }
-
    f1(i,m) if (Assigned[i] && !Choosed[i] && !Choosed[Assigned[i]]) 
    Choosed[i]=true;
 }
